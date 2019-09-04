27b5cc2555d5e3f4453d4fd8417b703150c7d632
anahisbarrie <anahis.barrie@gmail.com>
2019-09-04 19:15:01 -0400

    Updates

---
+++ precache-manifest.28d222ddd34a01565d77b87225e3d4f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89067ac6d676eeff311039ca91b7bba",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a7777cc9aba9b824bbc5",
+    "url": "/Clicky-Game/static/css/main.6a75e9c9.chunk.css"
+  },
+  {
+    "revision": "bf3af1b35f7b01fd9f57",
+    "url": "/Clicky-Game/static/js/2.234ebca3.chunk.js"
+  },
+  {
+    "revision": "a7777cc9aba9b824bbc5",
+    "url": "/Clicky-Game/static/js/main.654706a3.chunk.js"
+  },
+  {
+    "revision": "321debc0d9653ae72593",
+    "url": "/Clicky-Game/static/js/runtime~main.609595ed.js"
+  }
+]);
